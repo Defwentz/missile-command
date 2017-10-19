a7de830dc4766e6ddff174d05796dd6586c9bc1a
wzhu <macbookair@rrcs-108-169-151-40.midsouth.biz.rr.com>
2017-10-18 23:22:06 -0400

    vertex blinn-phong

--- rasterize.js
+++ rasterize.js
@@ -19,7 +19,11 @@ var LookUp = new vec4.fromValues(0.0,1.0,0.0,1.0);
 var gl = null; // the all powerful gl object. It's all here folks!
 var vertexBuffer; // this contains vertex coordinates in triples
 var triangleBuffer; // this contains indices into vertexBuffer in triples
-var colorBuffer;
+var ambientBuffer;
+var diffuseBuffer;
+var specularBuffer;
+var nBuffer;
+var normalBuffer;
 var triBufferSize = 0; // the number of indices in the triangle buffer
 var vertexPositionAttrib; // where to put position for vertex shader
 
@@ -84,7 +88,11 @@ function loadTrianglesnEllipsoids() {
     var whichSetTri; // index of triangle in current triangle set
     var coordArray = []; // 1D array of vertex coords for WebGL
     var indexArray = []; // 1D array of vertex indices for WebGL
-	var colorArray = [];
+	var ambientArray = [];
+	var diffuseArray = [];
+	var specularArray = [];
+	var normalArray = [];
+	var nArray = [];
     var vtxBufferSize = 0; // the number of vertices in the vertex buffer
     var vtxToAdd = []; // vtx coords to add to the coord array
     var indexOffset = vec3.create(); // the index offset for the current set
@@ -95,12 +103,22 @@ function loadTrianglesnEllipsoids() {
         for (var whichSet=0; whichSet<inputTriangles.length; whichSet++) {
             vec3.set(indexOffset,vtxBufferSize,vtxBufferSize,vtxBufferSize); // update vertex offset
             
+			var ambient = inputTriangles[whichSet].material.ambient;
+			var diffuse = inputTriangles[whichSet].material.diffuse;
+			var specular = inputTriangles[whichSet].material.specular;
+			var n = inputTriangles[whichSet].material.n;
             // set up the vertex coord array
             for (whichSetVert=0; whichSetVert<inputTriangles[whichSet].vertices.length; whichSetVert++) {
                 vtxToAdd = inputTriangles[whichSet].vertices[whichSetVert];
                 coordArray.push(vtxToAdd[0],vtxToAdd[1],vtxToAdd[2]);
-				var color = inputTriangles[whichSet].material.diffuse;
-				colorArray.push(color[0], color[1], color[2], 1.0);
+				
+				ambientArray.push(ambient[0], ambient[1], ambient[2], 1.0);
+				diffuseArray.push(diffuse[0], diffuse[1], diffuse[2], 1.0);
+				specularArray.push(specular[0], specular[1], specular[2], 1.0);
+				nArray.push(n);
+				
+				var normal = inputTriangles[whichSet].normals[whichSetVert];
+				normalArray.push(normal[0], normal[1], normal[2]);
             } // end for vertices in set
             
             // set up the triangle index array, adjusting indices across sets
@@ -117,7 +135,7 @@ function loadTrianglesnEllipsoids() {
 	
 	// part 2
     var inputElliposids = getJSONFile(INPUT_SPHERES_URL,"ellipsoids");
-	var numberofTri = 10; // this many slipt in both direction
+	var numberofTri = 19; // this many slipt in both direction
 
     if (inputElliposids != String.null) {
 		
@@ -126,11 +144,16 @@ function loadTrianglesnEllipsoids() {
             vec3.set(indexOffset,vtxBufferSize,vtxBufferSize,vtxBufferSize); // update vertex offset
             
 			var ellipsoid = inputElliposids[whichSet];
-			var color = ellipsoid.diffuse;
+			var ambient = ellipsoid.ambient;
+			var diffuse = ellipsoid.diffuse;
+			var specular = ellipsoid.specular;
+			var n = ellipsoid.n;
 			var a = ellipsoid.a;
 			var b = ellipsoid.b;
 			var c = ellipsoid.c;
-			
+			var asqr = a*a;
+			var bsqr = b*b;
+			var csqr = c*c;
             // set up the vertex coord array
             for (var i=0; i<=numberofTri; i++) {
 				var lat = -Math.PI/2 + Math.PI * i/numberofTri;
@@ -148,7 +171,30 @@ function loadTrianglesnEllipsoids() {
 					
 	                coordArray.push(vtxToAddx,vtxToAddy,vtxToAddz);
 					vtxBufferSize++;
-					colorArray.push(color[0], color[1], color[2], 1.0);
+					
+					ambientArray.push(ambient[0], ambient[1], ambient[2], 1.0);
+					diffuseArray.push(diffuse[0], diffuse[1], diffuse[2], 1.0);
+					specularArray.push(specular[0], specular[1], specular[2], 1.0);
+					nArray.push(n);
+					// // A = 1/a^2, B = 1/b^2, C = 1/c^2
+					// D = E = F = 0
+					// G = - 2*x_c/a^2, H = - 2*y_c/b^2, I = - 2*z_c/c^2
+					//
+					// xn = 2*A*xi + D*yi + E*zi + G
+					// yn = 2*B*yi + D*xi + F*zi + H
+					// zn = 2*C*zi + E*xi + F*yi + I
+					// =>
+					// xn = 2*A*xi + G
+					// yn = 2*B*yi + H
+					// zn = 2*C*zi + I
+
+					var nToAddx = 2 * (1./asqr) * vtxToAddx - 2 * ellipsoid.x / asqr;
+					var nToAddy = 2 * (1./bsqr) * vtxToAddy - 2 * ellipsoid.y / bsqr;
+					var nToAddz = 2 * (1./csqr) * vtxToAddz - 2 * ellipsoid.z / csqr;
+					
+					var normal = vec3.fromValues(nToAddx, nToAddy, nToAddz);
+					vec3.normalize(normal, normal);
+					normalArray.push(normal[0], normal[1], normal[2]);
 				}
             } // end for vertices in set
             // set up the vertex coord array
@@ -187,9 +233,25 @@ function loadTrianglesnEllipsoids() {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer); // activate that buffer
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indexArray),gl.STATIC_DRAW); // indices to that buffer
 
-	colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer); // activate that buffer
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorArray), gl.STATIC_DRAW);
+	ambientBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, ambientBuffer); // activate that buffer
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ambientArray), gl.STATIC_DRAW);
+	diffuseBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, diffuseBuffer); // activate that buffer
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(diffuseArray), gl.STATIC_DRAW);
+	specularBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, specularBuffer); // activate that buffer
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(specularArray), gl.STATIC_DRAW);
+	nBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer); // activate that buffer
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(nArray), gl.STATIC_DRAW);
+	
+	normalBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer); // activate that buffer
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalArray), gl.STATIC_DRAW);
+	
+	console.log(coordArray)
+	console.log(normalArray)
 } // end load triangles
 
 var shaderProgram;
@@ -201,26 +263,44 @@ function setupShaders() {
     var vShaderCode = `
 		uniform mat4 uMVMatrix;
 		uniform mat4 upMatrix;
-        attribute vec3 vertexPosition;
+		uniform mat3 uNMatrix;
 		
-		attribute vec4 aVertexColor;
+        attribute vec3 aVertexPosition;
+		attribute vec3 aVertexNormal;
+		attribute vec4 aVertexAmbient;
+		attribute vec4 aVertexDiffuse;
+		attribute vec4 aVertexSpecular;
+		attribute float aVertexN;
+		
 		varying vec4 vColor;
 		varying vec4 vPosition;
+		varying vec3 vLVec;
+		varying vec3 vNVec;
+		varying vec3 vVVec;
+		varying vec3 vHVec;
 		
+		uniform vec3 uEyePos;
 		uniform vec3 uLightPos;
 		uniform vec3 uLightAmbi;
 		uniform vec3 uLightDiff;
 		uniform vec3 uLightSepc;
-		
 
         void main(void) {
-			vec4 mvPos = uMVMatrix * vec4(vertexPosition, 1.0);
+			vec4 mvPos = uMVMatrix * vec4(aVertexPosition, 1.0);
             gl_Position = upMatrix * mvPos; // use the untransformed position
+			vPosition = vec4(aVertexPosition, 1.0);
 			
-			vColor = aVertexColor;
-			vPosition = vec4(vertexPosition, 1.0);
+			vLVec = normalize(uLightPos - mvPos.xyz);
+			vNVec = normalize(uNMatrix * aVertexNormal);
+			vVVec = normalize(uEyePos - mvPos.xyz);
+			vHVec = normalize(vVVec + vLVec);
 			
-			vec3 lightDirection = normalize(uLightPos - mvPos.xyz);
+			float NdotL = max(dot(vNVec, vLVec), 0.0);
+			float NdotH = max(dot(vNVec, vHVec), 0.0);
+			
+			vColor = aVertexAmbient*vec4(uLightAmbi,1.0)
+			+ aVertexDiffuse*vec4(uLightDiff,1.0) * NdotL
+ 			+ aVertexSpecular*vec4(uLightSepc,1.0) * pow(NdotH, aVertexN);
         }
     `;
 	
@@ -229,9 +309,15 @@ function setupShaders() {
 		precision mediump float;
 		varying vec4 vColor;
 		varying vec4 vPosition;
+		varying vec3 vLVec;
+		varying vec3 vNVec;
+		varying vec3 vVVec;
+		varying vec3 vHVec;
 		
         void main(void) {
             gl_FragColor = vColor;
+			
+			
         }
     `;
 	
@@ -263,16 +349,28 @@ function setupShaders() {
             } else { // no shader program link errors
                 gl.useProgram(shaderProgram); // activate shader program (frag and vert)
                 vertexPositionAttrib = // get pointer to vertex shader input
-                    gl.getAttribLocation(shaderProgram, "vertexPosition"); 
+                    gl.getAttribLocation(shaderProgram, "aVertexPosition"); 
                 gl.enableVertexAttribArray(vertexPositionAttrib); // input to shader from array
 				
 				
-				shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-				gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+				shaderProgram.vertexAmbientAttribute = gl.getAttribLocation(shaderProgram, "aVertexAmbient");
+				gl.enableVertexAttribArray(shaderProgram.vertexAmbientAttribute);
+				shaderProgram.vertexDiffuseAttribute = gl.getAttribLocation(shaderProgram, "aVertexDiffuse");
+				gl.enableVertexAttribArray(shaderProgram.vertexDiffuseAttribute);
+				shaderProgram.vertexSpecularAttribute = gl.getAttribLocation(shaderProgram, "aVertexSpecular");
+				gl.enableVertexAttribArray(shaderProgram.vertexSpecularAttribute);
+				shaderProgram.vertexNAttribute = gl.getAttribLocation(shaderProgram, "aVertexN");
+				gl.enableVertexAttribArray(shaderProgram.vertexNAttribute);
+				
+				
+				shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+				gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
 				
 				shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 				shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "upMatrix");
+				shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
 				
+				shaderProgram.eyePosUniform = gl.getUniformLocation(shaderProgram, "uEyePos");
 				shaderProgram.lightPosUniform = gl.getUniformLocation(shaderProgram, "uLightPos");
 				shaderProgram.lightAmbientUniform = gl.getUniformLocation(shaderProgram, "uLightAmbi");
 				shaderProgram.lightDiffuseUniform = gl.getUniformLocation(shaderProgram, "uLightDiff");
@@ -292,6 +390,10 @@ var pMatrix = mat4.create();
 function setMatrixUniforms() {
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+	
+	var nMatrix = mat3.create();
+	mat3.normalFromMat4(nMatrix, mvMatrix);
+	gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, nMatrix);
 }
 
 // render the loaded model
@@ -300,6 +402,12 @@ function renderScene() {
 	
 	var inputLights = getJSONFile(INPUT_LIGHTS_URL,"lights");
 	if(inputLights != String.null) {
+		gl.uniform3f(
+		                shaderProgram.eyePosUniform,
+		                Eye[0],
+		                Eye[1],
+		                Eye[2]
+		            );
 		for(var i=0; i<inputLights.length; i++) {
 			gl.uniform3f(
 			                shaderProgram.lightPosUniform,
@@ -307,6 +415,9 @@ function renderScene() {
 			                inputLights[i].y,
 			                inputLights[i].z
 			            );
+						console.log(inputLights[i].x,
+			                inputLights[i].y,
+			                inputLights[i].z)
 			gl.uniform3f(
 			                shaderProgram.lightAmbientUniform,
 			                inputLights[i].ambient[0],
@@ -355,10 +466,20 @@ function renderScene() {
     // vertex buffer: activate and feed into vertex shader
     gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer); // activate
     gl.vertexAttribPointer(vertexPositionAttrib,3,gl.FLOAT,false,0,0); // feed
+	
 	setMatrixUniforms();
 	
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, ambientBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexAmbientAttribute, 4, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, diffuseBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexDiffuseAttribute, 4, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, specularBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexSpecularAttribute, 4, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexNAttribute, 1, gl.FLOAT, false, 0, 0);
 	
     // triangle buffer: activate and render
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,triangleBuffer); // activate
